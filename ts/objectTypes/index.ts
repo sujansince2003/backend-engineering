362e68938caddfcb1c70dce48962a202378a9636
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-12-12 22:40:52 +0545

    add TypeScript example for object types and logging

---
+++ ts/objectTypes/index.ts
@@ -0,0 +1,20 @@
+let person: object
+
+
+person = {
+    name: "Alice",
+}
+
+console.log(typeof (person))
+
+
+
+const person1: {
+    name: string
+    isStudent: boolean
+} = {
+    name: "Sujan",
+    isStudent: true,
+}
+
+console.log(person1.name)
