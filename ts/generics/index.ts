086ed9426219747b7c3ab74ab2c5d4076c283171
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-12-14 08:27:43 +0545

    add generics example with getRandom function

---
+++ ts/generics/index.ts
@@ -0,0 +1,15 @@
+// generics
+
+
+
+function getRandom<T>(arr: T[]): T {
+    let randomIndex = 4
+    return arr[randomIndex]
+}
+
+
+let numbers = [1, 2, 4, 5, 6]
+// console.log(getRandom(numbers))
+
+
+
