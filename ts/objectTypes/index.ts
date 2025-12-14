56d5c573cd4e29819666990390718dc60fdca646
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-12-14 08:27:51 +0545

    add TypeScript examples for object manipulation, symbols, and enums

--- ts/objectTypes/index.ts
+++ ts/objectTypes/index.ts
@@ -18,3 +18,46 @@ const person1: {
 }
 
 console.log(person1.name)
+
+
+let person2: Object
+
+person2 = {
+    fname: "sujan"
+}
+console.log(typeof (person2))
+console.log(person2.valueOf())
+
+
+let sym = Symbol("sym")
+console.log(sym)
+
+const user = {
+    name: "Sujan",
+    [sym]: 123
+};
+
+console.log(user)
+
+
+enum Role {
+    admin,
+    user,
+    guest
+}
+
+console.log(typeof (Role))
+
+
+let a: (number[] | string)
+a = [1, 2, 3]
+a = "sujan"
+console.log(a)
+
+
+
+
+
+type Name = string
+let fname: Name = "sujan"
+console.log(`the name is ${fname}`)
