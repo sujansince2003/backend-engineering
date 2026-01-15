e71d7f2293a2b171232e0fc3785fa42cd8d06fe9
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2026-01-15 14:54:03 +0545

    feat: file handles

--- fs/index.mjs
+++ fs/index.mjs
@@ -1,4 +1,4 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
 async function readfile() {
   const data = await readFile("../heloo.txt", "utf-8");
@@ -6,4 +6,26 @@ async function readfile() {
   console.log("this is col", data);
 }
 
-readfile();
+// readfile();
+
+async function writetofile() {
+  try {
+    const data = await writeFile("test", "i am testing write", "utf-8");
+    console.log("file created and write successully");
+
+    const jsondata = {
+      name: "sujan",
+      age: 24,
+    };
+
+    const writejsondata = await writeFile(
+      "jsond.txt",
+      JSON.stringify(jsondata, null, 2)
+    );
+    console.log("write to json");
+  } catch (error) {
+    console.log("error when writing to file");
+  }
+}
+
+writetofile();
