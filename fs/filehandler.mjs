cc11ba99e6a34d988709003e7bdb9c5e3662ed72
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2026-01-15 22:40:44 +0545

    feat: file handlers

--- fs/filehandler.mjs
+++ fs/filehandler.mjs
@@ -1,5 +1,12 @@
 import fs from "fs/promises";
 
 async function fileHandles() {
-  const fileref = await fs.open("test.txt", 2);
+  const fileref = await fs.open("test.txt", "r+");
+  try {
+    const data = await fileref.read({});
+    console.log(data.buffer.toString("utf-8", 0, data.bytesRead));
+  } finally {
+    await fileref.close();
+  }
 }
+fileHandles();
